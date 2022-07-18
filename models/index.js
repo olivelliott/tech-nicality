aab6ea43b56ccd1812c20de488ea75d46bb43ac9
Olive <olivelliott48@gmail.com>
2022-07-18 12:04:49 -0400

    Create User model and connect to index.js

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+const Post = require('./Post');
+const Vote = require('./Vote');
+const Comment = require('./Comment');
+
+
+module.exports = { User, Post, Vote, Comment };
