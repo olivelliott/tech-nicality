f5515fdb7f0ef4f2e5d8b5d521c0f9effba08890
Olive <olivelliott48@gmail.com>
2022-07-18 22:22:40 -0400

    Create Comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,6 +1,51 @@
-// const router = require("express").Router();
-// const sequelize = require("../../config/connection");
-// const { Post, User, Comment, Vote } = require("../../models");
-// const withAuth = require("../../utils/auth");
+const router = require("express").Router();
+const sequelize = require("../../config/connection");
+const { Post, User, Comment, Vote } = require("../../models");
+const withAuth = require("../../utils/auth");
 
-// module.exports = router;
+// * /api/comments
+
+// GET all comments
+router.get("/", (req, res) => {
+    Comment.findAll()
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// CREATE a comment 
+router.post('/', withAuth, (req, res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.session.user_id,
+        post_id: req.body.post_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+    Comment.destroy({
+        where: { id: req.params.id }
+    })
+    .then(dbCommentData => {
+        if (!dbCommentData) {
+            res.status(404).json({ message: 'No comment found with this ID' });
+            return;
+        }
+        res.json(dbCommentData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
+
+
+module.exports = router;
