bf02e866ed707f20c03cb61e601cfcec69dd7fb6
Olive <olivelliott48@gmail.com>
2022-07-29 22:46:37 -0400

    Create comment seeds

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -76,6 +76,46 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post("/login", (req, res) => {
+  // expects {email: 'example@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: "You are now logged in!" });
+    });
+  });
+});
+
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+
 // UPDATE a user
 router.put('/:id', (req, res) => {
     // Only update what is passed through
