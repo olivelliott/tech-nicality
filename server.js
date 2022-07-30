c2ba99791606876469c00b689e3b332432c51496
Olive <olivelliott48@gmail.com>
2022-07-29 23:14:28 -0400

    Heroku deployment debug

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-  secret: process.env.DB_SECRET,
+  secret: 'secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
